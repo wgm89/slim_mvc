0632f74ff1f985e02a367cfcf78a280361b2a8b2
saeed <blandlove123@gmail.com>
2013-07-14 11:17:55 +0800

    request

--- Slim/Core/Controller.php
+++ Slim/Core/Controller.php
@@ -9,6 +9,8 @@ class Controller{
     public function __construct(){
         $this->view = new \Slim\View();
         $this->response = new \Slim\Http\Response();
+        $this->environment = \Slim\Environment::getInstance();
+        $this->request = new \Slim\Http\Request($this->environment);
     }
 
     public function render($template, $data = array())
